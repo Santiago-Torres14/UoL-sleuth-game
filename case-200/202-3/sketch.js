d0f746bfda095e7bb1ee86f14b2601c616bf0e5a
Santiago Torres Aragon <santiagotorres@freedomtorres.tech>
2023-06-24 19:38:10 -0500

    Solution for case 202 stage 3

---
+++ case-200/202-3/sketch.js
@@ -0,0 +1,302 @@
+/*
+
+Officer: 1061178
+CaseNum: 202-2-77850030-1061178
+
+Case 202 - The case of Bob and Daisy - stage 3
+
+Wow these two like to ham it up. Here’s the next letter. This time it’s from Bob (aka. Jobs).
+I need you to decode it to uncover more details about their plan.
+
+Discover the hidden code by commenting out all text commands except
+those which produce Medium Orchid filled text in Melissa font (see https://www.w3.org/TR/css3-iccprof#numerical).
+Only comment out text commands - leave fill & stroke commands uncommented.
+
+There are many possible ways of investigating this case, but you
+should use ONLY the following commands:
+
+  // comments are all that are needed for this case.
+  Do NOT add any new lines of code.
+
+*/
+
+var Ballpointprint;
+var Melissa;
+var Diggity;
+var RonsFont;
+
+
+function preload()
+{
+	Ballpointprint = loadFont('Ballpointprint.ttf');
+	Melissa = loadFont('Melissa.otf');
+	Diggity = loadFont('Diggity.ttf');
+	RonsFont = loadFont('RonsFont.ttf');
+}
+
+function setup()
+{
+	createCanvas(644,626);
+	textSize(32);
+}
+
+function draw()
+{
+	background(255);
+
+	fill(199,21,133);
+	textFont(RonsFont);
+	//text("so", 72,164);
+	fill(233,150,122);
+	//text("Daisy,", 142,32);
+	//text("to", 211,164);
+	fill(165,42,42);
+	textFont(Diggity);
+	//text("oti", 571,229);
+	textFont(Ballpointprint);
+	//text("at", 82,265);
+	fill(160,82,45);
+	//text("is.", 133,377);
+	fill(0,0,128);
+	textFont(Diggity);
+	//text("Bob", 11,505);
+	fill(124,252,0);
+	textFont(Ballpointprint);
+	//text("th", 111,377);
+	fill(30,144,255);
+	textFont(RonsFont);
+	//text("x", 61,505);
+	fill(255,140,0);
+	textFont(Diggity);
+	//text("I", 392,338);
+	fill(0,100,0);
+	textFont(Melissa);
+	//text("woods", 75,304);
+	textFont(Diggity);
+	//text("broadc", 353,229);
+	fill(127,255,212);
+	//text("Do", 11,96);
+	//text("ast", 424,229);
+	fill(238,130,238);
+	//text("ht", 472,164);
+	fill(220,20,60);
+	//text("the", 309,304);
+	fill(124,252,0);
+	textFont(Melissa);
+	//text("in", 323,164);
+	fill(144,238,144);
+	textFont(RonsFont);
+	//text("and", 141,304);
+	textFont(Melissa);
+	//text("I", 499,304);
+	fill(238,130,238);
+	textFont(Ballpointprint);
+	//text("the", 18,304);
+	fill(0,191,255);
+	//text("dream", 341,265);
+	fill(173,255,47);
+	textFont(Melissa);
+	//text("until", 349,338);
+	fill(154,205,50);
+	//text("all", 56,265);
+	textFont(RonsFont);
+	//text("not", 50,96);
+	fill(0,191,255);
+	//text("lovely", 68,32);
+	fill(107,142,35);
+	textFont(Diggity);
+	//text("to", 122,196);
+	fill(240,128,128);
+	textFont(Ballpointprint);
+	//text("we", 385,196);
+	fill(0,191,255);
+	textFont(Diggity);
+	//text("on", 597,229);
+	fill(107,142,35);
+	textFont(RonsFont);
+	//text("es,", 175,441);
+	fill(255,0,0);
+	//text("every", 120,265);
+	fill(255,105,180);
+	textFont(Melissa);
+	//text("We", 255,229);
+	fill(135,206,250);
+	//text("be", 342,96);
+	fill(123,104,238);
+	textFont(Diggity);
+	//text("I", 13,164);
+	fill(205,133,63);
+	textFont(RonsFont);
+	//text("shall", 292,229);
+	fill(199,21,133);
+	textFont(Melissa);
+	//text("old", 357,304);
+	fill(75,0,130);
+	textFont(Diggity);
+	//text("by", 273,304);
+	fill(32,178,170);
+	//text("temp", 109,164);
+	fill(139,0,139);
+	//text("rig", 449,164);
+	fill(238,232,170);
+	textFont(RonsFont);
+	//text("harbour.", 389,304);
+	fill(64,224,208);
+	textFont(Ballpointprint);
+	//text("down", 165,338);
+	fill(255,0,255);
+	textFont(Diggity);
+	//text("all", 81,377);
+	fill(186,85,211);
+	textFont(Melissa);
+	text("shooters", 352,129);
+	text("down", 440,129);
+	fill(128,0,0);
+	textFont(Diggity);
+	//text("and", 77,441);
+	fill(0,100,0);
+	textFont(RonsFont);
+	//text("!", 190,96);
+	fill(34,139,34);
+	textFont(Ballpointprint);
+	//text("no", 497,196);
+	fill(138,43,226);
+	//text("wine", 77,129);
+	textFont(RonsFont);
+	//text("playing", 256,129);
+	fill(0,128,128);
+	textFont(Ballpointprint);
+	//text("our", 465,265);
+	fill(255,140,0);
+	textFont(Melissa);
+	//text("away", 513,338);
+	fill(186,85,211);
+	text("at", 491,129);
+	text("arcade", 557,129);
+	text("the", 519,129);
+	fill(205,133,63);
+	textFont(Diggity);
+	//text("will", 303,96);
+	//text("Upon", 214,196);
+	//text("I", 325,265);
+	fill(199,21,133);
+	//text("we", 259,96);
+	fill(0,128,0);
+	textFont(Melissa);
+	//text("walks", 510,265);
+	fill(0,139,139);
+	textFont(RonsFont);
+	//text("days", 282,338);
+	textFont(Melissa);
+	//text("keep", 12,229);
+	fill(0,255,127);
+	textFont(Ballpointprint);
+	//text("Oh", 18,32);
+	fill(255,127,80);
+	textFont(Diggity);
+	//text("Love", 18,441);
+	fill(255,99,71);
+	textFont(Melissa);
+	//text("my", 272,196);
+	textFont(Ballpointprint);
+	//text("and", 199,129);
+	fill(210,105,30);
+	textFont(RonsFont);
+	//text("love", 108,229);
+	//text("raising", 369,96);
+	fill(255,255,0);
+	//text("now", 508,164);
+	fill(250,128,114);
+	textFont(Ballpointprint);
+	//text("opportunity.", 197,265);
+	fill(173,255,47);
+	//text("can", 408,338);
+	fill(123,104,238);
+	textFont(RonsFont);
+	//text("get", 459,338);
+	fill(124,252,0);
+	//text("k", 126,441);
+	fill(0,0,128);
+	//text("hidden.", 165,229);
+	fill(100,149,237);
+	//text("car", 390,164);
+	//text("of", 428,265);
+	fill(75,0,130);
+	textFont(Diggity);
+	//text("am", 29,164);
+	fill(0,0,139);
+	textFont(RonsFont);
+	//text("our", 472,229);
+	fill(25,25,112);
+	//text("run", 11,196);
+	fill(153,50,204);
+	//text("my", 344,164);
+	fill(255,105,180);
+	//text("longer", 535,196);
+	fill(160,82,45);
+	textFont(Diggity);
+	//text("despair", 104,96);
+	fill(218,112,214);
+	//text("toasts", 466,96);
+	fill(34,139,34);
+	//text("from", 17,377);
+	fill(135,206,250);
+	textFont(Ballpointprint);
+	//text("return", 305,196);
+	fill(65,105,225);
+	textFont(Diggity);
+	//text("have", 563,304);
+	//text("at", 547,96);
+	fill(135,206,235);
+	textFont(Melissa);
+	//text("back", 75,196);
+	fill(244,164,96);
+	textFont(Ballpointprint);
+	//text("down", 204,304);
+	fill(139,69,19);
+	textFont(Melissa);
+	//text("Jerrys", 10,129);
+	fill(72,209,204);
+	//text("and", 570,164);
+	fill(0,0,128);
+	textFont(Ballpointprint);
+	//text("you.", 158,196);
+	fill(139,69,19);
+	textFont(RonsFont);
+	//text("iss", 143,441);
+	fill(100,149,237);
+	//text("hall", 448,196);
+	fill(184,134,11);
+	textFont(Diggity);
+	//text("too", 516,304);
+	fill(154,205,50);
+	//text("our", 61,229);
+	//text("to", 20,265);
+	fill(0,255,127);
+	textFont(Ballpointprint);
+	//text("jump", 253,164);
+	fill(124,252,0);
+	textFont(Diggity);
+	//text("dev", 534,229);
+	//text("ted", 161,164);
+	//text("the", 234,338);
+	fill(218,112,214);
+	textFont(RonsFont);
+	//text("in", 571,265);
+	fill(220,20,60);
+	textFont(Melissa);
+	//text("begun", 12,338);
+	fill(240,128,128);
+	//text("Soon", 211,96);
+	fill(139,0,139);
+	textFont(RonsFont);
+	//text("s", 435,196);
+	fill(255,165,0);
+	textFont(Ballpointprint);
+	//text("counting", 66,338);
+	fill(220,20,60);
+	//text("bar", 146,129);
+
+
+
+}
