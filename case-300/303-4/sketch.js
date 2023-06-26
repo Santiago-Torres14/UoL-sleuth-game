90d45b78d150b62fb54e058cd4411d7c23c7a566
Santiago Torres Aragon <santiagotorres@freedomtorres.tech>
2023-06-25 20:14:21 -0500

    Solution for case 303 stage 4

---
+++ case-300/303-4/sketch.js
@@ -0,0 +1,258 @@
+/*
+
+Officer: 1061178
+CaseNum: 303-3-83562890-1061178
+
+Case 303 - The Case of the Crooked Attorney
+Stage 4 - The Courthouse
+
+Torvalds has his final safe in his courthouse chambers. Luckily there is a court case proceeding.
+You can sneak into his chambers whilst he makes his closing statement.
+
+Crack the safe by doing the following:
+
+	When the mouse button is pressed:
+	- Use the 'map' function to scale mouseY to values ranging from 3 to 18.
+	- Assign the output to Secret_Safe_Combination0
+
+	Whilst the mouse is being dragged:
+	- Use the 'random' function to produce random values ranging from 1 to 11.
+	- Assign the output to Secret_Safe_Combination1
+
+	When any key is released:
+	- Make Secret_Safe_Combination2 equal to the value of 'key'
+
+	Whilst the mouse is moving:
+	- Use the 'map' function to scale mouseX to values ranging from 13 to 77.
+	- Assign the output to Secret_Safe_Combination3
+
+	When the mouse button is released:
+	- Use the 'map' function to scale mouseX to values ranging from 17 to 69.
+	- Assign the output to Secret_Safe_Combination4
+
+	When the mouse button is released:
+	- Use the 'map' function to scale mouseX to values ranging from 15 to 80.
+	- Assign the output to Secret_Safe_Combination5
+
+
+
+This time you'll need to create the relevant event handlers yourself.
+
+There are many possible ways of investigating this case, but you
+should use ONLY the following commands:
+
+	- The assignment operator aka. the equals sign !
+	- mouseX, mouseY
+	- key, keyCode
+	- random
+	- map
+*/
+
+//declare the variables
+
+var Secret_Safe_Combination0;
+var Secret_Safe_Combination1;
+var Secret_Safe_Combination2;
+var Secret_Safe_Combination3;
+var Secret_Safe_Combination4;
+var Secret_Safe_Combination5;
+
+
+function preload()
+{
+	//IMAGES WILL BE LOADED HERE
+
+}
+
+/*
+	When the mouse button is pressed:
+	- Use the 'map' function to scale mouseY to values ranging from 3 to 18.
+	- Assign the output to Secret_Safe_Combination0
+
+	Whilst the mouse is being dragged:
+	- Use the 'random' function to produce random values ranging from 1 to 11.
+	- Assign the output to Secret_Safe_Combination1
+
+	When any key is released:
+	- Make Secret_Safe_Combination2 equal to the value of 'key'
+
+	Whilst the mouse is moving:
+	- Use the 'map' function to scale mouseX to values ranging from 13 to 77.
+	- Assign the output to Secret_Safe_Combination3
+
+	When the mouse button is released:
+	- Use the 'map' function to scale mouseX to values ranging from 17 to 69.
+	- Assign the output to Secret_Safe_Combination4
+
+	When the mouse button is released:
+	- Use the 'map' function to scale mouseX to values ranging from 15 to 80.
+	- Assign the output to Secret_Safe_Combination5
+*/
+function mousePressed() {
+     Secret_Safe_Combination0 = map(mouseY, 0, height, 3, 18, true);
+}
+
+function mouseDragged() {
+    Secret_Safe_Combination1 = random(1, 11);
+}
+
+function keyReleased() {
+    Secret_Safe_Combination2 = key;
+}
+
+function mouseMoved() {
+     Secret_Safe_Combination3 = map(mouseX, 0, width, 13, 77, true);
+}
+
+function mouseReleased() {
+    Secret_Safe_Combination4 = map(mouseX, 0, width, 17, 69, true);
+    Secret_Safe_Combination5 = map(mouseX, 0, width, 15, 80, true);
+}
+
+function setup()
+{
+	createCanvas(512,512);
+
+	//initialise the variables
+	Secret_Safe_Combination0 = 0;
+	Secret_Safe_Combination1 = "";
+	Secret_Safe_Combination2 = "";
+	Secret_Safe_Combination3 = 0;
+	Secret_Safe_Combination4 = 0;
+	Secret_Safe_Combination5 = 0;
+
+}
+
+///////////////////EVENT HANDLERS///////////////////
+
+//Create event handlers here to open the safe ...
+
+
+///////////////DO NOT CHANGE CODE BELOW THIS POINT///////////////////
+
+function draw()
+{
+
+	//Draw the safe door
+	background(70);
+	noStroke();
+	fill(29,110,6);
+	rect(26,26,width-52,width-52);
+
+	//Draw the combination dial
+	push();
+	translate(256,180);
+	drawDial(170,Secret_Safe_Combination0,20);
+	pop();
+
+	//Draw the spinners
+	push();
+	translate(206,280);
+	drawSpinner(3, Secret_Safe_Combination1);
+	pop();
+
+	push();
+	translate(306,280);
+	drawSpinner(3, Secret_Safe_Combination2);
+	pop();
+
+	//Draw the levers
+	push();
+	translate(125,356);
+	drawLever(Secret_Safe_Combination3);
+	pop();
+
+	push();
+	translate(250,356);
+	drawLever(Secret_Safe_Combination4);
+	pop();
+
+	push();
+	translate(375,356);
+	drawLever(Secret_Safe_Combination5);
+	pop();
+
+}
+
+function drawDial(diameter,num,maxNum)
+{
+	//the combination lock
+
+	var r = diameter * 0.5;
+	var p = r * 0.6;
+
+	stroke(0);
+	fill(255,255,200);
+	ellipse(0,0,diameter,diameter);
+	fill(100);
+	noStroke();
+	ellipse(0,0,diameter*0.66,diameter*0.66);
+	fill(150,0,0);
+	triangle(
+		-p * 0.4,-r-p,
+		p * 0.4,-r-p,
+		0,-r-p/5
+	);
+
+	noStroke();
+
+	push();
+	var inc = 360/maxNum;
+
+	rotate(radians(-num * inc));
+	for(var i = 0; i < maxNum; i++)
+	{
+		push();
+		rotate(radians(i * inc));
+		stroke(0);
+		line(0,-r*0.66,0,-(r-10));
+		noStroke();
+		fill(0);
+		text(i,0,-(r-10));
+		pop();
+	}
+
+	pop();
+}
+
+function drawLever(rot)
+{
+	push();
+	rotate(radians(-rot))
+	stroke(0);
+	fill(100);
+	rect(-10,0,20,100);
+	ellipse(0,0,50,50);
+	ellipse(0,100,35,35);
+	pop();
+}
+
+function drawSpinner(numSpinners, val)
+{
+	var sw = 20;
+	var ow = (sw + 5) * numSpinners + 5;
+	stroke(0);
+	fill(100);
+	rect(-ow/2,0,ow,35);
+	if(typeof(val) == "number")
+	{
+		val = floor(val).toString(); //convert to string
+	}
+	var d = numSpinners - val.length;
+
+	for(var d = numSpinners - val.length; d > 0; d--)
+	{
+		val = "-" + val;
+	}
+
+	for(var i = 0; i < numSpinners; i++)
+	{
+		stroke(0);
+		fill(255,255,200);
+		rect(-ow/2 + i * (sw + 5) + 5,5,20,25);
+		fill(0);
+		noStroke();
+		text(val[i],-ow/2 + sw/2 + i * (sw +5),25);
+	}
+
+}
