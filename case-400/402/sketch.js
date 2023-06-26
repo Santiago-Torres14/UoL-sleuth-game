2835c27f1ce6881edd7dc1453eed9d610cf4806e
Santiago Torres Aragon <santiagotorres@freedomtorres.tech>
2023-06-25 22:11:10 -0500

    Solution for case 401 stage 2

---
+++ case-400/402/sketch.js
@@ -0,0 +1,255 @@
+/*
+
+Officer: 1061178
+CaseNum: 401-1-38019854-1061178
+
+Case 401 - The Case of Norbert's Weiner Stand
+Stage 2 - Toxic Burrito
+
+Norbert is at it again. This time he’s set up a burrito stall and is lacing burritos with his foul toxin. The chaos is spreading. People are dropping like flies and burrito sales have fallen through the floor. To make matters worse it seems Norbert has cottoned on to our methods and has upped the complexity of his poison. You’ll find the antidote harder to develop this time. So kid, head down to the lab and get working.
+
+You must develop the antidote by using conditional statements in the draw loop to do the following:
+
+	- If sarin goes above 0.62, decrease sodium_bicarbonate by 0.04
+	- If methanol dips below 0.35 or botulinium goes above 0.75, increment sodium_bicarbonate by 0.01
+	- If botulinium dips below 0.49 or ricin dips below 0.45, decrement CalciumChloride by 0.01
+	- When methanol goes above 0.7, try increasing CalciumChloride by 0.03
+	- When botulinium goes above 0.44, decrement glucagon by 0.04
+	- When sarin dips below 0.47, increase glucagon by 0.01
+	- If methanol goes above 0.32 and ricin dips below 0.71, reduce antitoxin by 0.01
+	- When sarin dips below 0.39, increase antitoxin by 0.01
+
+
+Your conditional statements should consider the following poisons:
+
+	- botulinium
+	- methanol
+	- ricin
+	- sarin
+
+
+Your conditional statements should modify the following antidotes:
+
+	- sodium_bicarbonate
+	- CalciumChloride
+	- glucagon
+	- antitoxin
+
+
+- There are many ways to complete this task but you should only use the following commands and operators:
+
+	if(){}
+	>
+	<
+	&&
+	||
+	+=
+	-=
+
+*/
+
+//Declare the poison variables
+var botulinium;
+var methanol;
+var ricin;
+var sarin;
+
+
+//Declare the antidote variables
+var sodium_bicarbonate;
+var CalciumChloride;
+var glucagon;
+var antitoxin;
+
+
+//This variable is used for drawing the graph
+var graphs;
+
+
+function setup()
+{
+
+	createCanvas(800,600);
+	strokeWeight(2);
+
+	//initialise the poisons and antidotes
+	botulinium = 0.5;
+	methanol = 0.5;
+	ricin = 0.5;
+	sarin = 0.5;
+	sodium_bicarbonate = 0.5;
+	CalciumChloride = 0.5;
+	glucagon = 0.5;
+	antitoxin = 0.5;
+
+
+	//fills the graph with empty values
+	graphs = [];
+
+	for(var i = 0; i < 4; i++)
+	{
+		graphs.push([]);
+		for(var j = 0; j < 512; j++)
+		{
+			graphs[i].push(0.5);
+		}
+	}
+
+}
+
+function draw()
+{
+
+	//Develop the antidote below
+	//Write conditional statements to change the amount of each substance ...
+
+    /*
+	- If sarin goes above 0.62, decrease sodium_bicarbonate by 0.04
+	- If methanol dips below 0.35 or botulinium goes above 0.75, increment sodium_bicarbonate by 0.01
+	- If botulinium dips below 0.49 or ricin dips below 0.45, decrement CalciumChloride by 0.01
+	- When methanol goes above 0.7, try increasing CalciumChloride by 0.03
+	- When botulinium goes above 0.44, decrement glucagon by 0.04
+	- When sarin dips below 0.47, increase glucagon by 0.01
+	- If methanol goes above 0.32 and ricin dips below 0.71, reduce antitoxin by 0.01
+	- When sarin dips below 0.39, increase antitoxin by 0.01
+    */
+
+    if (sarin > 0.62) {
+        sodium_bicarbonate -= 0.04;
+    }
+
+    if (methanol < 0.35 || botulinium > 0.75) {
+        sodium_bicarbonate += 0.01;
+    }
+
+    if (botulinium < 0.49 || ricin < 0.45) {
+        CalciumChloride -= 0.01;
+    }
+
+    if (methanol > 0.7) {
+        CalciumChloride += 0.03;
+    }
+
+    if (botulinium > 0.44) {
+        glucagon -= 0.04;
+    }
+
+    if (sarin < 0.47) {
+        glucagon += 0.01;
+    }
+
+    if (methanol > 0.32 && ricin < 0.71) {
+        antitoxin -= 0.01;
+    }
+
+    if (sarin < 0.39) {
+        antitoxin += 0.01;
+    }
+
+
+	//////////////////////////////////////////////////////
+
+	//the code below generates new values using random numbers
+
+	/*
+		For testing, you might want to temporarily comment out
+		these lines and set the same variables to constant values
+		instead.
+	*/
+
+	botulinium = nextValue(graphs[0],botulinium);
+	methanol = nextValue(graphs[1],methanol);
+	ricin = nextValue(graphs[2],ricin);
+	sarin = nextValue(graphs[3],sarin);
+
+
+	sodium_bicarbonate = constrain(sodium_bicarbonate, 0, 1);
+	CalciumChloride = constrain(CalciumChloride, 0, 1);
+	glucagon = constrain(glucagon, 0, 1);
+	antitoxin = constrain(antitoxin, 0, 1);
+
+
+	///////// DO NOT CHANGE THE CODE BELOW ///////////
+
+	//drawing code
+
+	// set background
+	background(0);
+	noFill();
+
+	//draw the graphs for the vitals
+	var colors = [
+	color(255, 0, 0),
+	color(0, 255, 0),
+	color(0, 0, 255),
+	color(255, 0, 255),
+	color(255, 255, 0),
+	color(0, 255, 255)
+	];
+
+	for(var i = 0; i < graphs.length; i++)
+	{
+		stroke(colors[i]);
+		drawGraph(graphs[i]);
+	}
+
+	//draw the poisons as text
+	noStroke();
+	fill(colors[0]);
+	text('botulinium: ' + nf(botulinium,1,2), 20,20);
+	fill(colors[1]);
+	text('methanol: ' + nf(methanol,1,2), 20,40);
+	fill(colors[2]);
+	text('ricin: ' + nf(ricin,1,2), 20,60);
+	fill(colors[3]);
+	text('sarin: ' + nf(sarin,1,2), 20,80);
+
+
+	//draw the antidotes bar chart
+	drawBar(sodium_bicarbonate,50,'sodium_bicarbonate');
+	drawBar(CalciumChloride,200,'CalciumChloride');
+	drawBar(glucagon,350,'glucagon');
+	drawBar(antitoxin,500,'antitoxin');
+
+
+}
+
+function nextValue(graph, val)
+{
+	//gets the next value for a vital and puts it in an array for drawing
+	var delta = random(-0.03,0.03);
+
+	val += delta;
+	if(val > 1 || val < 0)
+	{
+		delta *= -1;
+		val += delta * 2;
+	}
+
+	graph.push(val)
+	graph.shift();
+	return val;
+}
+
+function drawGraph(graph)
+{
+	//draws an array as a graph
+	beginShape();
+	for(var i = 0; i < graph.length; i++)
+	{
+			vertex(width * i/512, height * 0.5 - graph[i]* height/3)
+	}
+	endShape();
+}
+
+
+function drawBar(val, x, name)
+{
+	//draws the bars for bar chart
+    noStroke();
+    fill(0,100,100);
+	var mh = height * 0.4 - 50;
+	rect(x,(height - 50) - val*mh, 100, val*mh);
+    fill(255);
+	text(name + ": " + val, x, height - 20);
+}
